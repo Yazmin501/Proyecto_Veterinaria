b0818f01c4354531638701d02d64e387f27eb8ec
Yazmin501 <utp0214254@alumno.utpuebla.edu.mx>
2026-07-18 11:52:53 -0600

    Listo Eliminar

--- FormEliminar.cs
+++ FormEliminar.cs
@@ -16,5 +16,60 @@ namespace Proyecto_Veterinaria
         {
             InitializeComponent();
         }
+
+        private void cmdMascotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Mascota seleccionada = (Mascota)cmdMascotas.SelectedItem;
+
+            if (seleccionada != null)
+            {
+                lbRaza.Text = "Raza: " + seleccionada.Raza;
+                lbEdad.Text = "Edad: " + seleccionada.Edad.ToString() + " años";
+                lbPeso.Text = "Peso: " + seleccionada.Peso.ToString() + " kg";
+                lbEspecie.Text = "Especie: " + seleccionada.Especie;
+            }
+        }
+
+        private void FormEliminar_Load(object sender, EventArgs e)
+        {
+            // Llenar ComboBox con nombres de mascotas
+            cmdMascotas.DataSource = Veterinaria.ObtenerMascotas();
+            cmdMascotas.DisplayMember = "Nombre"; // mostrar solo el nombre
+        }
+
+        private void btnEliminarMascota_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Mascota seleccionada = (Mascota)cmdMascotas.SelectedItem;
+
+                if (seleccionada != null)
+                {
+                    string mensaje =Veterinaria.Eliminar_Mascota(
+                        seleccionada.Nombre,
+                        seleccionada.Raza,
+                        seleccionada.Edad,
+                        seleccionada.Peso,
+                        seleccionada.Especie
+                    );
+
+                    MessageBox.Show(mensaje, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Refrescar ComboBox después de eliminar
+                    cmdMascotas.DataSource = null;
+                    cmdMascotas.DataSource = Veterinaria.ObtenerMascotas();
+                    cmdMascotas.DisplayMember = "Nombre";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void gpEliminar_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
